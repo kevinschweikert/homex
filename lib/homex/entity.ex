62aa664bd47d927010049d615cb57572beffaa77
Kevin Schweikert <kevin@kevinschweikert.de>
2025-07-10 23:17:11 +0200

    feat: add entity module

--- lib/homex/entity.ex
+++ lib/homex/entity.ex
@@ -2,6 +2,56 @@ defmodule Homex.Entity do
   @moduledoc """
   Defines the behaviour for an entity implementation
   """
+  @type t() :: %__MODULE__{
+          keys: MapSet.new(),
+          values: map(),
+          handlers: map(),
+          changes: map(),
+          private: nil
+        }
+
+  defstruct values: %{}, changes: %{}, handlers: %{}, keys: MapSet.new(), private: nil
+
+  def register_handler(
+        %__MODULE__{keys: keys, values: values, handlers: handlers} = entity,
+        key,
+        handler_fn,
+        initial_value \\ nil
+      )
+      when is_atom(key) do
+    values = Map.put(values, key, initial_value)
+    handlers = Map.put(handlers, key, handler_fn)
+    keys = MapSet.put(keys, key)
+
+    %{entity | keys: keys, values: values, handlers: handlers}
+  end
+
+  def put_change(%__MODULE__{keys: keys, changes: changes} = entity, key, value)
+      when is_atom(key) do
+    if key in keys do
+      changes = Map.put(changes, key, value)
+      %{entity | changes: changes}
+    end
+  end
+
+  def execute_change(
+        %__MODULE__{keys: keys, values: values, changes: changes, handlers: handlers} = entity
+      ) do
+    values =
+      for key <- keys, into: %{} do
+        value = Map.get(values, key)
+        change = Map.get(changes, key)
+        handler = Map.get(handlers, key)
+
+        if value != change and not is_nil(change) do
+          handler.(change)
+        end
+
+        {key, change}
+      end
+
+    %{entity | changes: %{}, values: values}
+  end
 
   @callback entity_id() :: String.t()
   @callback unique_id() :: String.t()
