8d1b139c8ed8038460fb88eb938f91ac031f2983
Kevin Schweikert <kevin@kevinschweikert.de>
2025-09-30 22:44:25 +0200

    chore: add package metadata

--- mix.exs
+++ mix.exs
@@ -1,14 +1,18 @@
 defmodule Homex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/kevinschweikert/homex"
+
   def project do
     [
       app: :homex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -19,9 +23,31 @@ defmodule Homex.MixProject do
     ]
   end
 
+  def cli do
+    [
+      preferred_envs: [
+        docs: :docs,
+        "hex.publish": :docs
+      ]
+    ]
+  end
+
+  def package do
+    [
+      description: "A bridge between Elixir and Homeassistant ",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => "https://hexdocs.pm/homex/changelog.html"
+      }
+    ]
+  end
+
   def docs do
     [
       main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
       extras: ["README.md", "CHANGELOG.md"],
       groups_for_modules: [
         Entities: [
@@ -38,6 +64,8 @@ defmodule Homex.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      # TODO: when https://github.com/emqx/emqtt/issues/289 is fixed:
+      # {:emqtt, "1.14.4", system_env: [{"BUILD_WITHOUT_QUIC", "1"}]},
       {:emqtt, github: "emqx/emqtt", tag: "1.14.4", system_env: [{"BUILD_WITHOUT_QUIC", "1"}]},
       {:ex_doc, "~> 0.38", only: :dev},
       {:jason, "~> 1.4"},
