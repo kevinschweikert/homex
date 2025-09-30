5692861ddce9bd761642e8c41fbdcc282a6352c5
Kevin Schweikert <kevin@kevinschweikert.de>
2025-09-30 20:54:20 +0200

    refactor: move everything config related to Homex.Config

--- lib/homex.ex
+++ lib/homex.ex
@@ -8,7 +8,7 @@ defmodule Homex do
 
   @impl Supervisor
   def init(_opts \\ []) do
-    config = config()
+    config = Homex.Config.get()
 
     children = [
       {DynamicSupervisor, name: Homex.EntitySupervisor, strategy: :one_for_one},
@@ -20,77 +20,6 @@ defmodule Homex do
     opts = [strategy: :rest_for_one, name: __MODULE__]
     Supervisor.init(children, opts)
   end
-
-  @config_schema [
-                   device: [
-                     default: [],
-                     required: false,
-                     type: :keyword_list,
-                     doc:
-                       "If no device configuration is given the identifiers and name will be set to the hostname of the device running Homex and will fall back to \"homex device\" when hostname is not available",
-                     keys: [
-                       identifiers: [required: false, type: {:or, [{:list, :string}, :mfa]}],
-                       name: [required: false, type: {:or, [:string, :mfa]}],
-                       manufacturer: [required: false, type: {:or, [:string, :mfa]}],
-                       model: [required: false, type: {:or, [:string, :mfa]}],
-                       serial_number: [required: false, type: {:or, [:string, :mfa]}],
-                       sw_version: [required: false, type: {:or, [:string, :mfa]}],
-                       hw_version: [required: false, type: {:or, [:string, :mfa]}]
-                     ]
-                   ],
-                   origin: [
-                     required: false,
-                     type: :non_empty_keyword_list,
-                     default: [name: "homex"],
-                     keys: [
-                       name: [
-                         required: false,
-                         type: :string,
-                         default: "homex",
-                         doc:
-                           "The name of the application that is the origin of the discovered MQTT item."
-                       ],
-                       sw_version: [
-                         required: false,
-                         type: :string,
-                         doc:
-                           "Software version of the application that supplies the discovered MQTT item"
-                       ],
-                       support_url: [
-                         required: false,
-                         type: :string,
-                         doc:
-                           "Support URL of the application that supplies the discovered MQTT item"
-                       ]
-                     ]
-                   ],
-                   discovery_prefix: [
-                     required: false,
-                     type: :string,
-                     default: "homeassistant",
-                     doc:
-                       "if changed in Homeassistant you also need to change it here to enable autodiscovery. The default works for a standard installation"
-                   ],
-                   entities: [required: false, default: [], type: {:list, :atom}],
-                   broker: [
-                     required: false,
-                     default: [],
-                     type: :keyword_list,
-                     keys: [
-                       host: [type: :string, default: "localhost", doc: "host of the MQTT broker"],
-                       port: [type: :integer, default: 1883, doc: "port of the MQTT broker"],
-                       username: [
-                         type: :string,
-                         doc: "username for the MQTT broker"
-                       ],
-                       password: [
-                         type: :string,
-                         doc: "passwort for the MQTT broker"
-                       ]
-                     ]
-                   ]
-                 ]
-                 |> NimbleOptions.new!()
 
   @moduledoc """
 
@@ -106,9 +35,8 @@ defmodule Homex do
     entities: [MyEntity]
   ```
 
-  The available options are:
+  The available options are documented in `Homex.Config`.
 
-  #{NimbleOptions.docs(@config_schema)}
 
   ## Usage
 
@@ -167,12 +95,6 @@ defmodule Homex do
   defdelegate add_entity(module), to: Homex.Manager
   defdelegate add_entities(modules), to: Homex.Manager
   defdelegate remove_entity(module), to: Homex.Manager
-
-  @doc false
-  def config_schema(), do: @config_schema
-
-  @doc false
-  def config, do: Application.get_all_env(:homex) |> Homex.Config.new()
 
   @doc false
   def hostname do
--- lib/homex/config.ex
+++ lib/homex/config.ex
@@ -1,7 +1,80 @@
 defmodule Homex.Config do
-  @moduledoc false
 
-  @type t() :: %__MODULE__{
+    @config_schema [
+                   device: [
+                     default: [],
+                     required: false,
+                     type: :keyword_list,
+                     doc:
+                       "If no device configuration is given the identifiers and name will be set to the hostname of the device running Homex and will fall back to \"homex device\" when hostname is not available",
+                     keys: [
+                       identifiers: [required: false, type: {:or, [{:list, :string}, :mfa]}],
+                       name: [required: false, type: {:or, [:string, :mfa]}],
+                       manufacturer: [required: false, type: {:or, [:string, :mfa]}],
+                       model: [required: false, type: {:or, [:string, :mfa]}],
+                       serial_number: [required: false, type: {:or, [:string, :mfa]}],
+                       sw_version: [required: false, type: {:or, [:string, :mfa]}],
+                       hw_version: [required: false, type: {:or, [:string, :mfa]}]
+                     ]
+                   ],
+                   origin: [
+                     required: false,
+                     type: :non_empty_keyword_list,
+                     default: [name: "homex"],
+                     keys: [
+                       name: [
+                         required: false,
+                         type: :string,
+                         default: "homex",
+                         doc:
+                           "The name of the application that is the origin of the discovered MQTT item."
+                       ],
+                       sw_version: [
+                         required: false,
+                         type: :string,
+                         doc:
+                           "Software version of the application that supplies the discovered MQTT item"
+                       ],
+                       support_url: [
+                         required: false,
+                         type: :string,
+                         doc:
+                           "Support URL of the application that supplies the discovered MQTT item"
+                       ]
+                     ]
+                   ],
+                   discovery_prefix: [
+                     required: false,
+                     type: :string,
+                     default: "homeassistant",
+                     doc:
+                       "if changed in Homeassistant you also need to change it here to enable autodiscovery. The default works for a standard installation"
+                   ],
+                   entities: [required: false, default: [], type: {:list, :atom}],
+                   broker: [
+                     required: false,
+                     default: [],
+                     type: :keyword_list,
+                     keys: [
+                       host: [type: :string, default: "localhost", doc: "host of the MQTT broker"],
+                       port: [type: :integer, default: 1883, doc: "port of the MQTT broker"],
+                       username: [
+                         type: :string,
+                         doc: "username for the MQTT broker"
+                       ],
+                       password: [
+                         type: :string,
+                         doc: "passwort for the MQTT broker"
+                       ]
+                     ]
+                   ]
+                 ]
+                 |> NimbleOptions.new!()
+
+  @moduledoc "#{NimbleOptions.docs(@config_schema)}"
+
+
+  @typep t() :: %__MODULE__{
           device: map(),
           origin: map(),
           discovery_prefix: String.t(),
@@ -17,8 +90,15 @@ defmodule Homex.Config do
 
   defstruct [:device, :origin, :discovery_prefix, :entities, :broker]
 
+
+  @doc false
+  def get, do: Application.get_all_env(:homex) |> new()
+
+
+  @doc false
+  @spec new(Keyword.t()) :: t()
   def new(opts) do
-    config = opts |> NimbleOptions.validate!(Homex.config_schema())
+    config = opts |> NimbleOptions.validate!(@config_schema)
 
     device = config |> make_device_config()
     origin = config |> make_origin_config()
@@ -68,7 +148,7 @@ defmodule Homex.Config do
     [
       name: Homex.EMQTT,
       host: String.to_charlist(config[:host]),
-      port: config[:broker][:port],
+      port: config[:port],
       username: optional(config[:username], &String.to_charlist/1),
       password: optional(config[:password], &String.to_charlist/1)
     ]
